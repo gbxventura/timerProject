1d4e562f89434e36be19096004106b2b2cb9d331
Gabriel Ventura <89049503+gbxventura@users.noreply.github.com>
2024-08-14 17:12:25 -0300

    mais aulas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Ola</h1>
+    <div className={styles.timerContent}>
+      <div className={styles.clock}>
+        <span>00:00:00:00</span>
       </div>
-    </>
+      <div className={styles.buttons}></div>
+      <div className={styles.checklist}></div>
+    </div>
   );
 }
 
